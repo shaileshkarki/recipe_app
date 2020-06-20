2d39d7e786288f77448f6015e14811154c125148
Shailesh <shlshkrk@gmail.com>
2020-06-20 20:18:49 +1000

    heroku upload changes

---
+++ public/precache-manifest.3022bdbf4c251d14743bf6c22ecedb3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142640fe387747edbdde544f226f5ccf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3139439fd7518748061",
+    "url": "/static/css/main.662945ce.chunk.css"
+  },
+  {
+    "revision": "0cfb2cb82a0f032ce052",
+    "url": "/static/js/2.23e2b67a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.23e2b67a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3139439fd7518748061",
+    "url": "/static/js/main.b2151f63.chunk.js"
+  },
+  {
+    "revision": "feeb7418a0faacc99f87",
+    "url": "/static/js/runtime-main.c939cca0.js"
+  },
+  {
+    "revision": "73990e9ee7ed6ad119a14c50e1cc7679",
+    "url": "/static/media/logo.73990e9e.svg"
+  }
+]);
