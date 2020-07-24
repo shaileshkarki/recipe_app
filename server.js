ae1ece2e63eaf03ef52dd8a4df9010d9be7614de
Shailesh <shlshkrk@gmail.com>
2020-07-24 13:56:31 +1000

    added an img to public folder

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 
 app.get('/', (req, res) => {
--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './Home.css';
-import axios from 'axios';
-import Card from './Card';
+import React from "react";
+import "./Home.css";
+import axios from "axios";
+import Card from "./Card";
 
 // const apiKey = process.env.SPOONACULAR_APIKEY;
 class Home extends React.Component {
   constructor(props) {
     super();
     this.state = {
-        query: "",
-        results: [],
-        isLoading: true,
-        userID: props.userID
-    }
+      query: "",
+      results: [],
+      isLoading: true,
+      userID: props.userID,
+    };
     // console.log(props);
   }
 
@@ -20,60 +20,61 @@ class Home extends React.Component {
     const apiKey = process.env.SPOONACULAR_APIKEY;
     // https://api.spoonacular.com/recipes/search?apiKey=0fe88fb2905449f68ae3dd65d0cf47c0&query=burger&number=10&includeInstruction=true
     const url = `https://api.spoonacular.com/recipes/search?apiKey=${apiKey}=${this.state.query}&number=5&includeInstruction=true`;
-    axios.get(url).then(res => {
+    axios.get(url).then((res) => {
       this.setState({
         // query: "",
         results: res.data,
-        isLoading: false
-      })
-    })
-    if(this.state.results.length!==0) {
-      axios.post('/recipe',this.state.results)
-      .then(function(response) {
+        isLoading: false,
+      });
+    });
+    if (this.state.results.length !== 0) {
+      axios.post("/recipe", this.state.results).then(function (response) {
         console.log(this.state.results);
-      })
+      });
     }
   }
 
   handleSearch = () => {
     const url = `https://api.spoonacular.com/recipes/search?apiKey=0fe88fb2905449f68ae3dd65d0cf47c0&query=${this.state.query}&number=5&includeInstruction=true`;
     // console.log(url);
-    axios.get(url).then(res => {
+    axios.get(url).then((res) => {
       // console.log(res.data.results);
       this.setState({
         results: res.data.results,
-        isLoading: false
-      })
-      if(res.data.results.length!==0) {
-        axios.post('/recipe',this.state.results)
-        .then(function(response) {
+        isLoading: false,
+      });
+      if (res.data.results.length !== 0) {
+        axios.post("/recipe", this.state.results).then(function (response) {
           // console.log(this.state.results);
-        })
+        });
       }
-    })
-  }
-    
+    });
+  };
+
   handleChange = (event) => {
     this.setState({
-      query: event.target.value
-    })
-  }
+      query: event.target.value,
+    });
+  };
 
   render() {
     console.log(this.props);
-    return(
-        <div className="home">
-            <div className="input-recipe">
-              <h1>Search Recipe:</h1>
-                <input type="text" onChange={this.handleChange}/>
-                <button onClick={this.handleSearch}>Search Recipe</button>
-            </div>
-            <div className="search-results">
-                <Card results={this.state.results} userID={this.state.userID} handleDeleteRecipe={this.props.handleDeleteRecipe}/>
-            </div>   
-                   
+    return (
+      <div className="home">
+        <div className="input-recipe">
+          <h1>Search Recipe:</h1>
+          <input type="text" onChange={this.handleChange} />
+          <button onClick={this.handleSearch}>Search Recipe</button>
         </div>
-    )
+        <div className="search-results">
+          <Card
+            results={this.state.results}
+            userID={this.state.userID}
+            handleDeleteRecipe={this.props.handleDeleteRecipe}
+          />
+        </div>
+      </div>
+    );
   }
 }
 export default Home;
